439b8618aa86e37cf32126f6fad85544de3a6949
felek <felek@DESKTOP-8AIIM5Q>
2025-01-02 21:17:35 +0300

    Add HttpHandler for working with Http)))

---
+++ FalseOS/System/OSUtlis/HttpHandler.cs
@@ -0,0 +1,69 @@
+﻿using Cosmos.System.Network.IPv4.UDP.DNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.System.Network.Config;
+using Cosmos.System.Network.IPv4;
+using CosmosHttp.Client;
+
+namespace FalseOS.System.OSUtlis
+{
+    public class HttpHandler
+    {
+        private static string GetPathFromURL(String url)
+        {
+            int start;
+            if (url.Contains("://"))
+                start = url.IndexOf("://") + 3;
+            else
+                start = 0;
+
+            int ios = url.IndexOf('/',start);
+            if (ios != -1) return url.Substring(ios);
+            else return "/";
+        }
+
+        public static String GetDomainNameFromURL(String url)
+        {
+            int start;
+            if (url.Contains("://"))
+                start = url.IndexOf("://") + 3;
+            else
+                start = 0;
+
+            int end = url.IndexOf("/",start);
+            if (end != -1)
+                end = url.Length;
+            return url[start..end];
+        }
+
+        public static byte[] downloadFile(String url)
+        {
+            if (url.StartsWith("http"))
+            {
+                WriteMessage.writeError("HTTPS protocol not supported yet!");
+                return "";
+            }
+
+            string path = GetPathFromURL(url);
+            string domainName = GetDomainNameFromURL(url);
+
+            var dns = new DnsClient();
+            dns.Connect(DNSConfig.DNSNameservers[0]);
+            dns.SendAsk(domainName);
+            Address addres = dns.Receive();
+            dns.Close();
+
+            HttpRequest rq = new();
+            rq.IP = addres.ToString();
+            rq.Domain = domainName;
+            rq.Path = path;
+            rq.Method = "GET";
+            rq.Send();
+
+            return rq.Response.GetStream();
+        }
+    }
+}
